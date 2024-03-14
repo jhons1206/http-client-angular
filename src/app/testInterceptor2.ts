bf2123683c042dd942d23abd90a545a7e9870a2e
jhons1206 <webdeveloper84@gmail.com>
2024-03-14 08:33:40 -0500

    Avance tema interceptors angular

---
+++ src/app/testInterceptor2.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+  HttpResponse,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class TestInterceptor2 implements HttpInterceptor {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    console.log(req);
+
+    return next.handle(req).do((event) => {
+      if (event instanceof HttpResponse) {
+        console.log(event);
+      }
+    });
+  }
+}
